49d87f93feff01df4910a2386fb9dfcc3f5a5c5c
Ara Winters <ara.winters@gmail.com>
2024-12-24 19:21:03 -0800

    build better npm package WIP

--- build.js
+++ build.js
@@ -5,6 +5,8 @@
 
 const fs    = require('fs');
 const path  = require('path');
+const glob  = require('glob');
+const replaceInFile = require('replace-in-file').replaceInFile;
 
 const _filesToExclude = [
     path.join('src','index.ts')
@@ -32,3 +34,78 @@ fs.cpSync(path.join('.','src'), path.join('.', ...'dist/@senzing/sz-sdk-nodejs-g
         console.error(err);
     }
 });
+
+
+// go through each module directory and rename the file(s) ending in "_pb.js/_pb.d.ts"
+// to "index.js"/"index.d.ts"
+function renameModuleFiles(inputGlob, outputName) {
+    return new Promise((resolve, reject) => {
+        let hasErrors = false;
+        glob(inputGlob, (err, files) => {
+            if (err) {
+              console.error('Error:', err);
+              hasErrors = true;
+              return;
+            }
+            //console.log(`files: `, files);
+        
+            files.forEach(file => {
+              //const filename  = (file.split(path.sep)).pop();
+              const fpath     = path.dirname(file);
+              const newPath   = path.join(fpath, outputName);
+              //console.log(`fpath: ${fpath}\n\rnew path: ${newPath}`);
+              
+              fs.renameSync(file, newPath, (err) => {
+                  if (err) {
+                    hasErrors = true;
+                    console.error('Error moving:', err);
+                  } else {
+                    //console.log('Moved:', file, 'to', newPath);
+                  }
+              });
+            });
+            if(hasErrors) {
+                reject();
+                return;
+            };
+            return resolve();
+        });
+    });    
+}
+
+// go through each module directory and rename the file(s) ending in "_gprc_pb.js"
+// to "gprc.js"
+renameModuleFiles("./dist/@senzing/sz-sdk-nodejs-grpc/**/**_grpc_pb.js", "grpc.js").then(
+    (res) => {
+        // now do the same thing for index files
+        renameModuleFiles("./dist/@senzing/sz-sdk-nodejs-grpc/**/**_pb.js", "index.js");
+        renameModuleFiles("./dist/@senzing/sz-sdk-nodejs-grpc/**/**_pb.d.ts", "index.d.ts");
+        
+        try {
+            const moduleNames = ['sz_product_pb.js']
+            const results = replaceInFile({
+                files:  [
+                    'dist/@senzing/sz-sdk-nodejs-grpc/**/grpc.js'
+                ],
+                from:   ["szconfig_pb.js","szconfigmanager_pb.js","szdiagnostic_pb.js","szengine_pb.js","szproduct_pb.js"],
+                to:     "index.js"
+            }).then((results) => {
+                if(results && results.forEach) {
+                    results.filter((rep) => { return rep.hasChanged }).forEach((rep) => {
+                        console.log(`renamed import path in "${rep.file}"`);
+                    });
+                }
+                //console.log('Replacement results:', results)
+            }).catch(err => {
+                console.error(err);
+            })
+        } catch (error) {
+            console.error('Error occurred:', error)
+        }
+    },
+    (err) => {
+        console.error(err);
+    }
+)
+
+
